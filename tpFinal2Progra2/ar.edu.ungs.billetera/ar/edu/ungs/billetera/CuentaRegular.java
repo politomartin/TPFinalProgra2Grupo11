1d7556d96b62357e2d2c117c5bd1c426ed5dc502
Tobias <ramireztobias1@gmail.com>
2026-05-28 18:03:01 -0300

    En CuentaRegular se agrego el limiteSaldo como dato

--- tpFinal2Progra2/ar.edu.ungs.billetera/ar/edu/ungs/billetera/CuentaRegular.java
+++ tpFinal2Progra2/ar.edu.ungs.billetera/ar/edu/ungs/billetera/CuentaRegular.java
@@ -1,6 +1,9 @@
 package ar.edu.ungs.billetera;
 
 public class CuentaRegular extends Cuenta {
+	
+	
+	 private double limiteSaldo = 5000000;
 
     public CuentaRegular(String dniUsuario, String alias) {
         super(0, dniUsuario, alias);
@@ -14,6 +17,17 @@ public class CuentaRegular extends Cuenta {
     @Override
     public boolean puedeInvertir(double monto) {
         return monto <= saldoDisponible * 0.5;
+    }
+    
+    @Override
+    public void depositar(double monto) {
+    	 if (saldoDisponible + monto > limiteSaldo) {
+
+             throw new IllegalStateException(
+                     "La cuenta regular supera el limite permitido");
+         }
+
+         saldoDisponible += monto;
     }
 
 	@Override
